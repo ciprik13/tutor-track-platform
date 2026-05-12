394d5b4b847d95e4ddbb6eeacba053b4cf58c2bf
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-05-12 17:21:10 +0300

    feat: register authSlice in redux store

--- apps/frontend/src/store/index.ts
+++ apps/frontend/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import profileReducer from '@/store/slices/profileSlice'
 import uiReducer from '@/store/slices/uiSlice'
+import authReducer from '@/store/slices/authSlice'
 
 export const store = configureStore({
   reducer: {
     profile: profileReducer,
     ui: uiReducer,
+    auth: authReducer,
   },
 })
 
